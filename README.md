85c870ea3a4dbbad51f7c7a9b76762b44b3502af
Bjoern Crombach <bjoern.crombach95@gmail.com>
2024-12-01 18:48:28 +0100

    add part two of day one

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 ```bash
-deno --allow-read ./src/advent-of-code-01.ts
+deno --allow-read ./src/advent-of-code-01-part-1.ts
+deno --allow-read ./src/advent-of-code-01-part-2.ts
 ```
---
+++ src/advent-of-code-01-part-2.ts
@@ -0,0 +1,69 @@
+const dir = "src/"
+const fileName = "advent-of-code-01-input.txt"
+
+main()
+
+async function main() {
+  const fileContent = await readInputFile(`${dir}${fileName}`)
+  const coordinateLists = prepareLists(fileContent)
+  const totalDistance = calculateTotalDistance(coordinateLists)
+  console.log("totalDistance: ", totalDistance)
+  const similarityScore = calculateSimilarityScore(coordinateLists)
+  console.log("similarityScore: ", similarityScore)
+}
+
+type CoordinateLists = {
+  leftList: number[]
+  rightList: number[]
+}
+
+async function readInputFile(filename: string) {
+  return await Deno.readTextFile(filename)
+}
+
+function prepareLists(fileContent: string) {
+  const rows = fileContent.split("\n")
+
+  const leftList: number[] = []
+  const rightList: number[] = []
+  const listSeparator = "   "
+
+  for (const row of rows) {
+    leftList.push(parseInt(row.split(listSeparator)[0]))
+    rightList.push(parseInt(row.split(listSeparator)[1]))
+  }
+
+  leftList.sort((a, b) => a - b)
+  rightList.sort((a, b) => a - b)
+
+  return { leftList, rightList }
+}
+
+function calculateTotalDistance(coordinateLists: CoordinateLists) {
+  const { leftList, rightList } = coordinateLists
+  const length = leftList.length
+
+  const locationDistances: number[] = []
+  for (let index = 0; index < length; index++) {
+    locationDistances.push(Math.abs(leftList[index] - rightList[index]))
+  }
+
+  return locationDistances.reduce((distanceSum, distance) => distanceSum + distance, 0)
+}
+
+function calculateSimilarityScore(coordinateLists: CoordinateLists) {
+  const { leftList, rightList } = coordinateLists
+  const similarityScores: number[] = []
+
+  for (const leftEntry of leftList) {
+    let apperances = 0
+    for (const rightEntry of rightList) {
+      if (leftEntry === rightEntry) {
+        apperances++
+      }
+    }
+    similarityScores.push(leftEntry * apperances)
+  }
+
+  return similarityScores.reduce((similaritySum, similarityScore) => similaritySum + similarityScore, 0)
+}
